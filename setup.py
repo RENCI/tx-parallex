20bb4afe49b9e92cbc697f456823dacfd15ec811
Hao Xu <xuh@cs.unc.edu>
2020-07-14 23:58:51 -0400

    override log_level via env var

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
         name="tx-parallex",
-        version="0.0.40",
+        version="0.0.41",
         license="MIT",
         author="Hao Xu",
         author_email="xuhao@renci.org",
