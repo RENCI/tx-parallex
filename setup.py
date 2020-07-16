e37c66a1bc262de69a169de0c998e0b95b57282f
Hao Xu <xuh@cs.unc.edu>
2020-07-16 17:00:02 -0400

    add destructure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
         name="tx-parallex",
-        version="0.0.44",
+        version="0.0.45",
         license="MIT",
         author="Hao Xu",
         author_email="xuhao@renci.org",
