1efc26c97166352711d041c37cdbd7622345f432
Hao Xu <xuh@cs.unc.edu>
2020-08-20 06:51:12 -0400

    --amend

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md", "r") as fh:
             "joblib==0.16.0"
         ],
         extras_require=[
-            "shm": [
+            "pimos": [
                 "pyarrow==1.0.0"
             ]
         ]
