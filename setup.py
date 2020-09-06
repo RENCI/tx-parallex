034431e5dd49f64b8dcfb338a90923b6a3cfb33f
Hao Xu <xuh@cs.unc.edu>
2020-09-06 04:36:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
     setuptools.setup(
         name="tx-parallex",
-        version="0.1.9",
+        version="0.1.10",
         license="MIT",
         author="Hao Xu",
         author_email="xuhao@renci.org",
